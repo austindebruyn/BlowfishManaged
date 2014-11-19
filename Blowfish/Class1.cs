c6ca8d5baeafa19f8f3122736550bb354d2a62df
Austin deBruyn <austin.debruyn@gmail.com>
2014-11-18 18:33:13 -0600

    initial commit

---
+++ Blowfish/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blowfish
+{
+    public class Class1
+    {
+        public static String hello = "Hello world";
+    }
+}
