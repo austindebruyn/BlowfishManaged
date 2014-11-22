aa0dabdeb55cff5a29b2a714eeaafe83ba7012c1
Austin deBruyn <austin.debruyn@gmail.com>
2014-11-21 20:05:53 -0600

    working demo with benchmark

---
+++ Blowfish/ByteOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blowfish
+{
+    class ByteOperations
+    {
+        /// <summary>
+        /// Convert a hex string into an array of bytes.
+        /// </summary>
+        /// <param name="hexString"></param>
+        /// <returns></returns>
+        public static byte[] ConvertHexStringToByteArray(string hexString)
+        {
+            if (hexString.Length % 2 != 0)
+            {
+                throw new ArgumentException(String.Format(System.Globalization.CultureInfo.InvariantCulture, "The binary key cannot have an odd number of digits: {0}", hexString));
+            }
+
+            byte[] HexAsBytes = new byte[hexString.Length / 2];
+            for (int index = 0; index < HexAsBytes.Length; index++)
+            {
+                string byteValue = hexString.Substring(index * 2, 2);
+                HexAsBytes[index] = byte.Parse(byteValue, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture);
+            }
+
+            return HexAsBytes;
+        }
+    }
+}
